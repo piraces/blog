460a9eef52b69a42319c23c4586229542ac6c6fc
piraces <raul.piraces@gmail.com>
2020-09-19 19:50:31 +0200

    Solve problems with PWA script

---
+++ js/pwa.js
@@ -0,0 +1,8 @@
+if ("serviceWorker" in navigator) {
+    navigator.serviceWorker
+        .register("/service-worker.js")
+        .then(function () {
+            console.log("ServiceWorker has been registered!");
+        })
+        .catch(console.error);
+}
