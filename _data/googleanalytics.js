433a7fd6c45ce74c977a49d3238b21c59cd3f8d0
piraces <raul.piraces@gmail.com>
2020-09-22 16:44:06 +0200

    Fix google analytics

--- _data/googleanalytics.js
+++ _data/googleanalytics.js
@@ -23,7 +23,7 @@ const metadata = require("./metadata.json");
 
 module.exports = function () {
   let id = metadata.googleAnalyticsId;
-  if (/UA-136840526-1/.test(id)) {
+  if (/Update me/.test(id)) {
     id = null;
   }
   return id;
